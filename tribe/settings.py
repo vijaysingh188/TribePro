0295b3a760eb0df7fe69214404a73a31b4da2003
unknown <dunchanmaster007@gmail.com>
2024-11-06 19:20:13 +0530

    added docker

--- tribe/settings.py
+++ tribe/settings.py
@@ -76,20 +76,14 @@ WSGI_APPLICATION = 'tribe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-        
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
+
 
 
 
